a3b489263e58d641c5b23d4a98d820198f830a18
Ex0rnius <nixer974@gmail.com>
2025-10-23 09:45:32 -0600

    Add files via upload

---
+++ Unidad3Practica1Problema19/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Unidad3Practica1Problema19
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(30, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(736, 40);
+            label1.TabIndex = 0;
+            label1.Text = "Calificaciones, Promedio, Aprobación y Reprobación";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(30, 70);
+            label2.Name = "label2";
+            label2.Size = new Size(150, 30);
+            label2.TabIndex = 1;
+            label2.Text = "Calificación 1:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(30, 126);
+            label3.Name = "label3";
+            label3.Size = new Size(150, 30);
+            label3.TabIndex = 2;
+            label3.Text = "Calificación 2:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(125, 342);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 30);
+            label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(30, 183);
+            label5.Name = "label5";
+            label5.Size = new Size(150, 30);
+            label5.TabIndex = 3;
+            label5.Text = "Calificación 3:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(30, 237);
+            label6.Name = "label6";
+            label6.Size = new Size(150, 30);
+            label6.TabIndex = 4;
+            label6.Text = "Calificación 4:";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.Location = new Point(253, 67);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(183, 35);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox2.Location = new Point(253, 121);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(183, 35);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox3.Location = new Point(253, 180);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(183, 35);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox4.Location = new Point(253, 234);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(183, 35);
+            textBox4.TabIndex = 8;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(326, 305);
+            button1.Name = "button1";
+            button1.Size = new Size(148, 67);
+            button1.TabIndex = 9;
+            button1.Text = "Revisar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.grid_paper_pattern_background_vector;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button1;
+    }
+}
---
+++ Unidad3Practica1Problema19/Form1.cs
@@ -0,0 +1,26 @@
+namespace Unidad3Practica1Problema19
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double c1, c2, c3, c4, promedio;
+            c1 = double.Parse(textBox1.Text);
+            c2 = double.Parse(textBox2.Text);
+            c3 = double.Parse(textBox3.Text);
+            c4 = double.Parse(textBox4.Text);
+            promedio = (c1 + c2 + c3 + c4) / 4;
+            if
+                (promedio >= 6 && c1 >= 6 && c2 >= 6 && c3 >= 6 && c4 >= 6) MessageBox.Show("El alumno está aprobado");
+            else
+                MessageBox.Show("El alumno está reprobado");
+
+
+        }
+    }
+}
