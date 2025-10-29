eb4d140f8bd8a1a0cc200a52020003d344be31cd
Ex0rnius <nixer974@gmail.com>
2025-10-29 10:01:06 -0600

    Add files via upload

---
+++ Unidad3Practica2Problema8/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Unidad3Practica2Problema8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            textBox1 = new TextBox();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(27, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(284, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Factorial de un Número";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox1.Location = new Point(348, 37);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 39);
+            textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 21;
+            listBox1.Location = new Point(329, 82);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(148, 298);
+            listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(521, 194);
+            button1.Name = "button1";
+            button1.Size = new Size(147, 56);
+            button1.TabIndex = 3;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(27, 394);
+            label2.Name = "label2";
+            label2.Size = new Size(254, 32);
+            label2.TabIndex = 4;
+            label2.Text = "Factorial del Número";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(410, 394);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 32);
+            label3.TabIndex = 5;
+            label3.Text = "es";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox2.Location = new Point(300, 391);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 39);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox3.Location = new Point(489, 391);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(141, 39);
+            textBox3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.grid_paper_pattern_background_vector;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private ListBox listBox1;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private TextBox textBox3;
+    }
+}
